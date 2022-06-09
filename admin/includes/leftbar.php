52dc484c4af2dee819f5d56cba873cddb529a98d
ZubaerJewel <Zubaer5Jewel.16@gmail.com>
2022-06-09 15:24:21 +0600

    Remove this page link from project .

--- admin/includes/leftbar.php
+++ admin/includes/leftbar.php
@@ -1,27 +1,65 @@
-	<nav class="ts-sidebar " style="background-color:red" >
-			<ul class="ts-sidebar-menu">
-			
-				<li class="ts-label text-white">Main</li>
-				<li><a href="dashboard.php"><i class="fa fa-dashboard text-white"></i> Dashboard</a></li>
-			
-<li><a href="#"><i class="fa fa-files-o  text-white"></i> Blood Group</a>
-<ul>
-<li><a href="add-bloodgroup.php">Add Blood Group</a></li>
-<li><a href="manage-bloodgroup.php">Manage Blood Group</a></li>
-</ul>
-</li>
+		
+			<style>
+	.logo img{
+		width:150px;
+		height:70px;
+		float:left;
+	}
+	nav {
+		width:;
+	}
+</style> 
+    <style>
+    .navbar-toggler {
+        z-index: 1;
+    }
+    
+    @media (max-width: 576px) {
+        nav > .container {
+            width: 100%;
+        }
+    }
+    </style>
 
+ <nav class="ts-sidebar navbar-toggleable-md  bg-danger">
+        <button class="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon  "><i class="fa fa-desktop"></i></span>
+        </button>
+		
+		
+        <div class="container ">
 
-				<li><a href="add-donor.php"><i class="fa fa-edit text-white"></i> Add Donor</a></li>
-				<li><a href="donor-list.php"><i class="fa fa-users text-white" ></i> Donor List</a></li>
+            <div class="collapse" id="navbarExample">
+                <ul class="ts-sidebar-menu  btn-group  ml-auto ">
 				
-				<li><a href="add-agent.php"><i class="fa fa-edit text-white"></i> Add Agent</a></li>
-				<li><a href="agent-list.php"><i class="fa fa-users text-white" ></i> Agent List</a></li>
+								<li class="ts-label text-white my-5" style="font-size:22px">Main</li>
+								<li><a class="btn" style="font-size:20px" href="dashboard.php"><i class="fa fa-dashboard text-white"></i> Dashboard</a></li>
+			
+								<li><a class="btn" style="font-size:20px"  href="#"><i class="fa fa-files-o  text-white"></i> Blood</a>
+					<ul>
+								<li><a class="btn" style="font-size:20px" href="add-bloodgroup.php">Add Blood</a></li>
+								<li><a class="btn" style="font-size:20px"  href="manage-bloodgroup.php">Manage Blood</a></li>
+					</ul>
+								</li>
 
-				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop text-white"></i> Messages</a></li>
-			<li><a href="manage-pages.php"><i class="fa fa-files-o text-white"></i> Manage Pages</a></li>
-			<li><a href="update-contactinfo.php"><i class="fa fa-files-o text-white"></i> Update Contact</a></li>
 
+								<li><a class="btn btn-primary-hover" style="font-size:20px" href="add-donor.php"><i class="fa fa-edit text-white"></i> Add Donor</a></li>
+								<li><a class="btn" style="font-size:20px" href="donor-list.php"><i class="fa fa-users text-white" ></i> Donor List</a></li>
+				
+								<li><a class="btn" style="font-size:20px"  href="add-agent.php"><i class="fa fa-edit text-light"></i> Add Agent</a></li>
+								<li><a class="btn" style="font-size:20px"  href="agent-list.php"><i class="fa fa-users text-white" ></i> Agent List</a></li>
 
-			</ul>
-		</nav>
+								<li><a class="btn" style="font-size:20px"  href="manage-conactusquery.php"><i class="fa fa-envelope text-white" ></i>Messages</a></li>
+								<li><a class="btn" style="font-size:20px"  href="manage-pages.php"><i class="fa fa-files-o text-white"></i> Manage Pages</a></li>
+								<li><a class="btn" style="font-size:20px"  href="update-contactinfo.php"><i class="fa fa-files-o text-white"></i> Update Contact</a></li>
+								<li><a class="btn" style="font-size:20px"  href="change-password.php">Change Password</a></li>
+								<li><a class="btn" style="font-size:20px"  href="logout.php">Logout</a></li>
+				
+
+                 
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+	
